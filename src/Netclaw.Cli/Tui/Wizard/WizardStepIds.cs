c0b62348260ad3b2e476cd1f3b57d09fd8e4e607
Aaron Stannard <aaron@petabridge.com>
2026-04-29 00:42:57 +0000

    chore: add copyright headers to new files

--- src/Netclaw.Cli/Tui/Wizard/WizardStepIds.cs
+++ src/Netclaw.Cli/Tui/Wizard/WizardStepIds.cs
@@ -1,3 +1,8 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="WizardStepIds.cs" company="Petabridge, LLC">
+//      Copyright (C) 2026 - 2026 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
 namespace Netclaw.Cli.Tui.Wizard;
 
 internal static class WizardStepIds
